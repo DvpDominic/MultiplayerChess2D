6db22091bc1d174e60c60d234e0f72c11c99b2fe
Darshan Parmar <parmardarshanv@gmail.com>
2025-05-02 00:14:59 +0530

    Adding tiles and material

--- Assets/Game/Scripts/ChessBoard.cs
+++ Assets/Game/Scripts/ChessBoard.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 
 public class ChessBoard : MonoBehaviour
 {
-    // Inputs
+    [Header("Art")]
+    [SerializeField] private Material tileMaterial;
+    
+    // Constraints
     private const int TILE_COUNT_X = 8;
     private const int TILE_COUNT_Y = 8;
     private GameObject[,] tiles;
@@ -29,6 +32,24 @@ public class ChessBoard : MonoBehaviour
     {
         GameObject tileObject = new GameObject(string.Format("X:{0},Y:{1}", x, y));
         tileObject.transform.parent = transform;
+
+        Mesh mesh = new Mesh();
+        tileObject.AddComponent<MeshFilter>().mesh = mesh;
+        tileObject.AddComponent<MeshRenderer>().material = tileMaterial;
+
+        Vector3[] vertices = new Vector3[4];
+        vertices[0] = new Vector3(x * tileSize, y * tileSize);
+        vertices[1] = new Vector3(x * tileSize, (y+1) * tileSize);
+        vertices[2] = new Vector3((x+1) * tileSize, y * tileSize);
+        vertices[3] = new Vector3((x+1) * tileSize, (y+1) * tileSize);
+
+        int[] tris = new int[]{ 0, 1, 2, 1, 3, 2 };
+        
+        mesh.vertices = vertices;
+        mesh.triangles = tris;
+
+        tileObject.AddComponent<BoxCollider2D>();
+        
         
         return tileObject;
     }
